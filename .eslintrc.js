c46110db943a5352a6ac83944e7cbb9d37e77623
fj <menspocky322@gmail.com>
2022-10-10 12:27:08 +0900

    ESLint に Airbnb ルールを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,14 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir : __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb-typescript',
     'plugin:prettier/recommended',
   ],
   root: true,
@@ -17,6 +19,9 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    // クラスメソッドが this を利用するように強制するルールをオフにする（https://eslint.org/docs/latest/rules/class-methods-use-this）
+    'class-methods-use-this': 'off',
+    // デフォルトで設定されているルール
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
